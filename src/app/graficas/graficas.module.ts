0a2ea7a16a6bac58969a4a789067b4d0f825fdd4
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-27 00:20:03 -0600

    Importanto chart.js y ng2-charts y diseñando barras.component

--- src/app/graficas/graficas.module.ts
+++ src/app/graficas/graficas.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChartsModule } from 'ng2-charts';
 
 import { GraficasRoutingModule } from './graficas-routing.module';
 import { BarrasComponent } from './pages/barras/barras.component';
@@ -18,6 +19,7 @@ import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.
   ],
   imports: [
     CommonModule,
+    ChartsModule,
     GraficasRoutingModule
   ]
 })
