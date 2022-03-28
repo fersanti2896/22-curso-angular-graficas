33e7ef985173b5c71d8b9f361e7f238f528a056f
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-28 12:24:37 -0600

    Mostrando la información desde el backend - Segunda Forma

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -28,18 +28,23 @@ export class DonaHttpComponent implements OnInit {
   constructor(private serviceGraficas: GraficasService) { }
 
   ngOnInit(): void {
-    this.serviceGraficas.getUsuariosRedesSociales()
-        .subscribe( data => {
-          console.log(data);
-          const labels = Object.keys( data );
-          const values = Object.values( data );
-        
-          console.log(labels);
-          console.log(values);
+    /* Primera Forma */
+    // this.serviceGraficas.getUsuariosRedesSociales()
+    //     .subscribe( data => {
+    //       console.log(data);
+    //       const labels = Object.keys( data );
+    //       const values = Object.values( data );
 
+    //       this.doughnutChartLabels = labels;
+    //       this.doughnutChartData.push(values);
+    //     });
+
+    /* Segunda Forma */
+    this.serviceGraficas.getUsuariosRedesSocialesDonaData()
+        .subscribe( ({ labels, values }) => {
           this.doughnutChartLabels = labels;
           this.doughnutChartData.push(values);
-        });
+        })
   }
 
 }
