8866dceb3b0c0c8b26958eb869e13088b61eea77
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-28 12:13:39 -0600

    Mostrando la información desde el backend - Primera Forma

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
 import { GraficasService } from '../../services/graficas.service';
 
 @Component({
@@ -9,12 +11,34 @@ import { GraficasService } from '../../services/graficas.service';
 })
 export class DonaHttpComponent implements OnInit {
 
+  public doughnutChartLabels: Label[] = [ 
+    // 'CETES', 'Supertasas', 'Kubo Financiero', 'Finsus', 'GBM+'
+  ];
+  public doughnutChartData: MultiDataSet = [
+      // [ 9, 26, 13, 34, 17 ] 
+  ];
+
+  public doughnutChartType: ChartType = 'doughnut';
+  public colors: Color[] = [
+    { 
+      backgroundColor: [ '#60ACE0', '#24E04C', '#FF6150', '#FAC241', '#9460E0' ]
+     }
+  ]
+
   constructor(private serviceGraficas: GraficasService) { }
 
   ngOnInit(): void {
     this.serviceGraficas.getUsuariosRedesSociales()
         .subscribe( data => {
           console.log(data);
+          const labels = Object.keys( data );
+          const values = Object.values( data );
+        
+          console.log(labels);
+          console.log(values);
+
+          this.doughnutChartLabels = labels;
+          this.doughnutChartData.push(values);
         });
   }
 
