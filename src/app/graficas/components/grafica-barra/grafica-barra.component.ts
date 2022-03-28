34d6b9ae7b7854bcac4f50f7d7ec9f4cfeea114d
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-28 01:22:59 -0600

    Editando grafica-barra.component para mostrar gráficos en barras-doble.component

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica-barra',
@@ -7,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class GraficaBarraComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public barChartOptions: ChartOptions = {
+    responsive: true
+  };
+
+  public barChartLabels: Label[] = ['2016', '2017', '2018', '2019', '2020', '2021', '2022'];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+
+  public barChartData: ChartDataSets[]  = [
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor: '#4B3FE6', hoverBackgroundColor: 'grey' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B', backgroundColor: '#7AF52C',hoverBackgroundColor: 'grey' },
+      { data: [ 8, 45, 80, 49, 36, 67, 50 ], label: 'Series C', backgroundColor: '#FF304C', hoverBackgroundColor: 'grey' }
+  ];
 }
--- src/app/graficas/pages/barras-doble/barras-doble.component.ts
+++ src/app/graficas/pages/barras-doble/barras-doble.component.ts
@@ -7,10 +7,8 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class BarrasDobleComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
