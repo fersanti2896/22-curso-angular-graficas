992229bf4a5dcf98febb52be56784154539e2858
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-28 11:57:19 -0600

    Gráfica de dona por http

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GraficasService } from '../../services/graficas.service';
 
 @Component({
   selector: 'app-dona-http',
@@ -8,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonaHttpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private serviceGraficas: GraficasService) { }
 
   ngOnInit(): void {
+    this.serviceGraficas.getUsuariosRedesSociales()
+        .subscribe( data => {
+          console.log(data);
+        });
   }
 
 }
